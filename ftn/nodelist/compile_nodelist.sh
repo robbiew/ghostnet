a1276cd90941d01caa8747b4d17f34389bb9a37a
RobbieW <robbiew@gmail.com>
2024-06-28 16:02:35 +0000

    Post compile auto-commit: 2024-06-28 16:02:30

--- ftn/nodelist/compile_nodelist.sh
+++ ftn/nodelist/compile_nodelist.sh
@@ -4,8 +4,8 @@
 # https://github.com/christiansacks/tqwnet_nodelist/
 
 ORIGDIR=$PWD
-IWORKDIR="/home/bbs/git/ghostnet/ftn/nodelist"
-IPACKDIR="/home/bbs/git/ghostnet/ftn/infopack"
+IWORKDIR="/root/git/ghostnet/ftn/nodelist"
+IPACKDIR="/root/git/ghostnet/ftn/infopack"
 WORKDIR="${1:-$IWORKDIR}"
 PACKDIR="${2:-$IPACKDIR}"
 
